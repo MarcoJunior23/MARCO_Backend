742b328a80e1170908e8fc4ead9d4233c5037b04
Marcos <marcos97795@gmail.com>
2024-08-14 21:46:03 -0300

    A

---
+++ AULA03/ex1-string.js
@@ -0,0 +1,20 @@
+const estudante = "Daniel"; // String
+let num = 5; // variavel numerica
+let texto = estudante + num; // adiciona a variavel estudante com a variavel num
+let stexto = `nome ${estudante} numero ${num}`; // exemplo template string
+//console.log(texto);
+console.log(stexto);
+const cifrao = '\u0024'; // simbolo utilizando codigo utf
+const aMaiusculo = '\u0041'; // simbolo utilizando codigo utf
+const tique = '\u2705'; // simbolo utilizando codigo utf
+const hiragana = '\u3041'; // simbolo utilizando codigo utf
+let tel_estudante = null; // variavel com valor vazio
+let cpf = undefined ;
+console.log(cpf);
+console.log(tel_estudante);
+console.log(cifrao);
+console.log(aMaiusculo);
+console.log(tique);
+console.log(hiragana);
+console.log(stexto.length); // nome da string . lenght retorna o tamanho da string
+console.log(texto.length); // retorna o tamanho da string 
---
+++ AULA03/ex7.js
@@ -0,0 +1,5 @@
+const texto = "senai backend aula 03";
+let stexto = texto.toUpperCase;
+const texto2 = "Senai desenvolvimento de sistemas"
+console.log(stexto);
+console.log(texto2.toLowerCase()); // converte o texto para minusculo
