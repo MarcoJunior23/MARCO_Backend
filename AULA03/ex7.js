6460eb898f95c8358493e24be38a0c05631d431a
Marco <marcos97795@gmail.com>
2024-08-21 19:36:07 -0300

    aa

--- AULA03/ex7.js
+++ AULA03/ex7.js
@@ -1,5 +1,10 @@
 const texto = "senai backend aula 03";
-let stexto = texto.toUpperCase;
-const texto2 = "Senai desenvolvimento de sistemas"
+let stexto = texto.toUpperCase(); // converte o texto para maiusculo
+const texto2 = "SENAI DESENVOLVIMENTO DE SISTEMAS";
 console.log(stexto);
 console.log(texto2.toLowerCase()); // converte o texto para minusculo
+
+
+
+
+
