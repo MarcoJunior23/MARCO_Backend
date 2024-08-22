41cec3e29265971b557ed856b9ffbccc97a1074c
Marco <marcos97795@gmail.com>
2024-08-21 22:38:03 -0300

    a
    a

--- Aula04/ex5.js
+++ Aula04/ex5.js
@@ -1,29 +1,29 @@
-class Usuario{
-    constructor(){
-        this.nome=null;
+class Usuario {
+    constructor() {
+        this.nome = null;
         this.idade;
     }
-    exibeinfo(){
+    exibeinfo() {
         console.log(`Nome ${this.nome}, idade ${this.idade}`);
     }
 }
-class cliente extends Usuario{
-    constructor(){
+class cliente extends Usuario {
+    constructor() {
         super();
-               
+
 
         this.cargo = null;
     }
-    exibeinfocargo(){
+    exibeinfocargo() {
         console.log(`Nome ${this.nome}, idade ${this.idade}, cargo ${this.cargo}`);
     }
 
 }
 
 const Daniel = new cliente();
-Daniel.nome="Daniel";
+Daniel.nome = "Daniel";
 Daniel.idade = 29;
-Daniel.cargo= "Professor";
+Daniel.cargo = "Professor";
 const Daniel1 = new Usuario();
 Daniel1.nome = "Filipe";
 Daniel1.idade = 29;
