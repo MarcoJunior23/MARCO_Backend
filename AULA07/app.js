cc1ebcbf3f69b55726c7bd5ec8e6e6bfc4ec5036
Marco Junior <marcos97795@gmail.com>
2024-09-11 22:20:57 -0300

    Create app.js

---
+++ AULA07/app.js
@@ -0,0 +1,14 @@
+const express = require('express') // variavel express que vai salvar uma requisição
+const app = express() // variavel app armazena o express
+const port = 3000 // porta padrão para as aplicaçoes em node
+
+// cria uma requisição get
+
+/*app.get('/',(req,res)=>{
+    res.send('Hello world')
+
+});
+*/
+// app post é para fazer um post na api com express
+app.get('/usuario',(req,res)=>)
+   res.send('usuario ')
